88985a73a8ca5a7f7f488705ad7c5197593216de
Denis Nasonov <brain0991@gmail.com>
2023-10-28 20:47:51 +0300

    fix: bug fix

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -80,8 +80,8 @@ function extractNameFromTemplate(value) {
  *   'John Doe'  => 'J'
  *   'cat'       => 'c'
  */
-function getFirstChar(value) {
-
+function getFirstChar(/* value */) {
+  throw new Error('Not implemented');
 }
 
 /**
